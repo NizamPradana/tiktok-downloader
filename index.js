1303fef98307c4cf9db8868d54cd1fec0abb2868
Nizam Pradana <nizampradanaxrpl1@gmail.com>
2026-06-09 22:58:48 +0700

    apus poling

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 require('./src/bot');
-console.log('🤖 Bot TikTok Downloader aktif!');
+
+// Tambahkan express untuk handle webhook
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.post(`/bot${process.env.BOT_TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+  console.log('🤖 Bot TikTok Downloader aktif!');
+});
+app.listen(process.env.PORT || 3000);
